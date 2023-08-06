d6224ec6de07ee708317aa9555fb987cbc5eaed0
Vinayak Sharma <1871vinayak@gmail.com>
2023-08-06 06:23:41 +0530

    Code Fixed
    Fixed the errors and made the important changes.

--- src/component/list/ListRow.jsx
+++ src/component/list/ListRow.jsx
@@ -1,8 +1,8 @@
 import styles from "./ListRow.module.css";
 
-{/*const ListCell = ({ children }) => {
-  return <tr className={styles.cell}>{children}</tr>;
-};*/}
+// const ListCell = ({ children }) => {
+//   return <tr className={styles.cell}>{children}</tr>;
+// };
 const ListCell = ({ children,...props }) => {
   return <tr className={styles.cell}{...props}>{children}</tr>;
 };
